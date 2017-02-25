4b142eac6f637a6c745410734d7f8be3993429c3
nodza <noel.hwande@gmail.com>
2017-02-25 11:05:19 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+User.create!(email: "noel@safirio.com", first_name: "King", last_name: "Kunta", password: "zxcvbnm", password_confirmation: "zxcvbnm")
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: 1)
 end
 
 puts "100 posts created"
