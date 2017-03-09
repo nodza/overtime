17fae1fd5c088fe342735df4b5ce938e8c78119f
nodza <noel.hwande@gmail.com>
2017-03-09 15:40:03 -0600

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 User.create!(email: "noel@safirio.com", first_name: "King", last_name: "Kunta", password: "zxcvbnm", password_confirmation: "zxcvbnm")
 
+puts "1 user created"
+
+AdminUser.create!(email: "admin@allvirtuous.com", first_name: "Emperor", last_name: "Augustus", password: "zxcvbnm", password_confirmation: "zxcvbnm")
+
+puts "1 admin user created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: 1)
 end
